89fafe624e5f6b90853f83a31bd3202abb30ca35
LorenzoTorchio <lorenzotorchios@gmail.com>
2023-04-09 20:19:52 -0300

    Agrego las excepciones MontoNegativo y CarritoCero, altero la clase carrito para que use una collecion.

--- Java/Programas/Carrito/Carrito/src/ListaDeCompras.java
+++ Java/Programas/Carrito/Carrito/src/ListaDeCompras.java
@@ -9,15 +9,10 @@ import org.junit.jupiter.api.Assertions;
 
 
 public class ListaDeCompras {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, Carrito.CarritoCeroException, Carrito.MontoNegativoException {
     Path archivo = Paths.get("lista.txt");
 
-    long l = Files.lines(archivo).count();
-
-    int dimension = (int) l;
-
-    Carrito carrito = new Carrito(dimension);
-    int index = 0;
+    Carrito carrito = new Carrito();
 
         for (String linea : Files.readAllLines(archivo, StandardCharsets.UTF_8)) {
         String[] i = linea.split(" ");
@@ -27,31 +22,40 @@ public class ListaDeCompras {
 
         Producto producto = new Producto(nombre, precio);
         ItemCarrito item = new ItemCarrito(producto, cantidad);
-        carrito.setItem(index, item);
-        index++;
-    }
+        carrito.addItem(item);
+        }
 
         if(args.length > 0){
-            String tipoDescuento = args[0];
-            switch (tipoDescuento) {
-                case "Fijo" -> {
-                    Descuento desc = new Descuento.Fijo();
-                    desc.setValorDesc(Float.parseFloat(args[1]));
-                    desc.valorFinal(carrito.getPrecio());
-                    System.out.println("el precio de su compra es de $" +  desc.valorFinal(carrito.getPrecio()));
+            try {
+                String tipoDescuento = args[0];
+                switch (tipoDescuento) {
+                    case "Fijo" -> {
+                        Descuento desc = new Descuento.Fijo();
+                        desc.setValorDesc(Float.parseFloat(args[1]));
+                        desc.valorFinal(carrito.Precio());
+                        carrito.setPrecio(desc.valorFinal(carrito.Precio()));
+                    }
+                    case "Porcentaje" -> {
+                        Descuento desc = new Descuento.Porcentaje();
+                        desc.setValorDesc(Float.parseFloat(args[1]));
+                        desc.valorFinal(carrito.Precio());
+                        carrito.setPrecio(desc.valorFinal(carrito.Precio()));
+                    }
+                    case "PorcentajeTope" -> {
+                        Descuento.PorcentajeTope desc = new Descuento.PorcentajeTope();
+                        desc.setValorDesc(Float.parseFloat(args[1]));
+                        desc.setTope(Float.parseFloat(args[2]));
+                        carrito.setPrecio(desc.valorFinal(carrito.Precio()));
+                    }
                 }
-                case "Porcentaje" -> {
-                    Descuento desc = new Descuento.Porcentaje();
-                    desc.setValorDesc(Float.parseFloat(args[1]));
-                    desc.valorFinal(carrito.getPrecio());
-                    System.out.println("el precio de su compra es de $" +  desc.valorFinal(carrito.getPrecio()));
-                }
-                case "PorcentajeTope" -> {
-                    Descuento.PorcentajeTope desc = new Descuento.PorcentajeTope();
-                    desc.setValorDesc(Float.parseFloat(args[1]));
-                    desc.setTope(Float.parseFloat(args[2]));
-                    System.out.println("el precio de su compra es de $" +  desc.valorFinal(carrito.getPrecio()));
-                }
+
+            } catch (Carrito.MontoNegativoException e){
+                System.out.println("Descuento resulta en un valor negativo");
+            } catch (Carrito.CarritoCeroException e){
+                System.out.println("Descuento no aplicable");
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
             }
         }
 
@@ -59,17 +63,19 @@ public class ListaDeCompras {
         TestDescuento();
     }
 
-    private static void TestDescuento(){
-        Carrito carrito1 = new Carrito(3);
-        carrito1.setItem(0, new ItemCarrito(new Producto("papa", 130), 3));
-        carrito1.setItem(1, new ItemCarrito(new Producto("desodorante", 2000), 2));
-        carrito1.setItem(2, new ItemCarrito(new Producto("yerba", 600), 1));
-        Assertions.assertEquals(4990.0,carrito1.getPrecio());
 
-        Carrito carrito2 = new Carrito(2);
-        carrito2.setItem(0, new ItemCarrito(new Producto("jabon",400),1));
-        carrito2.setItem(1, new ItemCarrito(new Producto("cuchillo",700),2));
-        Assertions.assertEquals(1800.0,carrito2.getPrecio());
+
+    private static void TestDescuento() throws Carrito.MontoNegativoException, Carrito.CarritoCeroException {
+        Carrito carrito1 = new Carrito();
+        carrito1.addItem(new ItemCarrito(new Producto("Producto 1", 130), 3));
+        carrito1.addItem(new ItemCarrito(new Producto("Producto 2", 2000), 2));
+        carrito1.addItem(new ItemCarrito(new Producto("Producto 3", 600), 1));
+        Assertions.assertEquals(4990.0,carrito1.Precio());
+
+        Carrito carrito2 = new Carrito();
+        carrito2.addItem(new ItemCarrito(new Producto("Producto 1",400),1));
+        carrito2.addItem(new ItemCarrito(new Producto("Producto 2",700),2));
+        Assertions.assertEquals(1800.0,carrito2.Precio());
 
         float ValorInicial = 1300.0F;
         Descuento desc1 = new Descuento.Fijo();
@@ -84,6 +90,14 @@ public class ListaDeCompras {
         desc3.setValorDesc(10.0F);
         desc3.setTope(5.0F);
         Assertions.assertEquals(1235.0,desc3.valorFinal(ValorInicial));
+
+        Carrito carrito3 = new Carrito();
+        carrito3.addItem(new ItemCarrito(new Producto("Producto 1", 100), 0));
+        Assertions.assertThrows(Carrito.CarritoCeroException.class, carrito3::Precio);
+
+        Carrito carrito4 = new Carrito();
+        carrito4.addItem(new ItemCarrito(new Producto("Producto 1", -100), 1));
+        Assertions.assertThrows(Carrito.MontoNegativoException.class, carrito4::Precio);
     }
 
 }
--- Java/Programas/Carrito/Carrito/src/paquete/Carrito.java
+++ Java/Programas/Carrito/Carrito/src/paquete/Carrito.java
@@ -1,30 +1,61 @@
 package paquete;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Carrito {
-    private final ItemCarrito[] carrito;
+    private final List<ItemCarrito> items;
+    private float precio;
 
 
-    public Carrito(int espacio) {
-        this.carrito = new ItemCarrito[espacio];
+    public Carrito() {
+        this.items = new ArrayList<>();
     }
 
-    public void setItem(int i, ItemCarrito item) {
-        carrito[i] = item;
+    public void addItem(ItemCarrito item) {
+        items.add(item);
+    }
+
+    public float getPrecio(){
+        return this.precio;
     }
 
 
-    public float getPrecio() {
+    public float Precio() throws CarritoCeroException, MontoNegativoException {
+
         float precio = 0;
-        for (ItemCarrito itemCarrito : carrito) {
+        for (ItemCarrito itemCarrito : items) {
             precio += itemCarrito.precioItem();
         }
+
+        if (precio == 0) {
+            throw new CarritoCeroException();
+        }
+
+
         return precio;
     }
+
+    public static class CarritoCeroException extends Exception {
+        public CarritoCeroException() {
+        }
+    }
+
+    public static class MontoNegativoException extends Exception {
+        public MontoNegativoException() {
+        }
+    }
+
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
 
     @Override
     public String toString() {
         String lista = "";
-        for (ItemCarrito itemCarrito : carrito) {
+        for (ItemCarrito itemCarrito : items) {
             lista = lista + itemCarrito.toString() + "\n";
         }
         return lista;
