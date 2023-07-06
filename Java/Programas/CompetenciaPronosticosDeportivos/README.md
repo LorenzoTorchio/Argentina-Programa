f6d982abbfe2edc9b55cefbbff956964b7f3dcc6
LorenzoTorchio <lorenzotorchios@gmail.com>
2023-07-06 15:58:47 -0300

    segregacion de archivos del curso

--- Java/Programas/CompetenciaPronosticosDeportivos/README.md
+++
@@ -1,4 +0,0 @@
-# App
-Trabajo Practico Integrador Competencia de Pronosticacion de Partidos
-
-comando de ejecucion: java -jar target\competenciaPronosticos-jar-with-dependencies.jar resultados.csv configuracion.txt
--- Java/Programas/ConversionCSV/src/main/java/LorenzoTorchio/Main.java
+++
@@ -1,7 +0,0 @@
-package LorenzoTorchio;
-//https://dzone.com/articles/how-to-convert-csv-to-json-in-java
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
